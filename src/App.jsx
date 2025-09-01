5f4bbaf71103a9af302883ae295825e80e1f32b6
system sylvere BARINDA <systembarinda@gmail.com>
2025-09-02 00:25:21 +0200

    get everything now is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Skills_Occupations from "./Pages/Skills_Occupations"
 import SearchOccupations from "./Pages/SearchOccupations"
+import SearchSkills from "./Pages/SearchSkills"
 
 function App() {
   
 
   return (
     <>
-    
+    <SearchSkills />
       </>
   )
 }
