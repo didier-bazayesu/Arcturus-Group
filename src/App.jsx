8118ee0c108c8acd75e729fef8d0689d1097454a
didier-bazayesu <didierbazayesu@gmail.com>
2025-09-02 23:06:07 +0200

    fix: merge new with

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { BrowserRouter as Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HandleLogin from "./Pages/Login";
 import HandleSignUp from "./Pages/Signup";
 import AboutUs from "./Pages/About";
 import Footer from "./Pages/Footer"
 import MainLandingPage from "./Pages/MainLandingPage"
-import { BrowserRouter,Routes, Route } from "react-router-dom"
-
 import ShortReport from "./Pages/shortReport"
 import { Navigation } from "./Pages/navigation"
 
