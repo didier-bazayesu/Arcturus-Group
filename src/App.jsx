2bc19d5c25f3d522f3aea9400b3273c8b06cba98
system sylvere BARINDA <systembarinda@gmail.com>
2025-09-03 22:53:54 +0200

    the Install the express,cors,csv-parse that use to run in nodejs server

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-
-
-
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HandleLogin from "./Pages/Login";
 import HandleSignUp from "./Pages/Signup";
@@ -9,27 +6,31 @@ import Footer from "./Pages/Footer"
 import MainLandingPage from "./Pages/MainLandingPage"
 import ShortReport from "./Pages/shortReport"
 import { Navigation } from "./Pages/navigation"
+import SearchAndOccupations from "./Pages/skillsAndOccupa"
 
 
 function App() {
 
   return (
 
- 
+ <>
+   <SearchAndOccupations />
+   
+    // <BrowserRouter>
+    //   <Navigation />
+    //   <Routes>
+    //       <Route path="/login" element={<HandleLogin />} />
+    //       <Route path="/" element={<Navigate to="/login" />} />
+    //       <Route path="/signup" element={<HandleSignUp />} />
+    //       <Route path="/About" element={<AboutUs/>}/>
+    //       <Route path="/Report" element={<ShortReport />} />
+    //       <Route path="/about" element={<MainLandingPage />} />
+    //   </Routes>
+    //   <Footer/> 
+    // </BrowserRouter>
 
-    <BrowserRouter>
-      <Navigation />
-      <Routes>
-          <Route path="/login" element={<HandleLogin />} />
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path="/signup" element={<HandleSignUp />} />
-          <Route path="/About" element={<AboutUs/>}/>
-          <Route path="/Report" element={<ShortReport />} />
-          <Route path="/about" element={<MainLandingPage />} />
-      </Routes>
-      <Footer/> 
-    </BrowserRouter>
-    
+    </>
+
   );
 
 }
