baf6fc8c0d41b7186ccf394e760095551ed2607b
system sylvere BARINDA <systembarinda@gmail.com>
2025-09-05 22:58:56 +0200

    the option  skills are done

--- src/Pages/skillsAndOccupa.jsx
+++ src/Pages/skillsAndOccupa.jsx
@@ -130,14 +130,14 @@ const SearchAndOccupations = () => {
               )}
             </ul>
           </div>
-          <div className="bg-white dark:bg-gray-800 p-6 rounded-2xl shadow-lg">
+          <div className="bg-white dark:bg-gray-100 p-6 rounded-2xl shadow-lg">
             <h3 className="text-xl font-bold mb-4">
               <span className="text-orange-600">Related Skills</span>
             </h3>
             <ul className="list-disc pl-5 space-y-2">
               {relatedSkills.length > 0 ? (
                 relatedSkills.map(skill => (
-                  <li key={skill.id} className="text-gray-700 dark:text-gray-300">
+                  <li key={skill.id} className="text-gray-900 dark:text-gray-900">
                     <span className="font-semibold">{skill.label}</span>
                   </li>
                 ))
