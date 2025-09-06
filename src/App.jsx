1b4c700cad5d11b2c1267f4ac9f6376c10d11157
system sylvere BARINDA <systembarinda@gmail.com>
2025-09-06 11:30:43 +0200

    the assential complite the design of the differents of pages of explores the skill and occupation

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,26 @@ import Footer from "./Pages/Footer"
 import MainLandingPage from "./Pages/MainLandingPage"
 import ShortReport from "./Pages/shortReport"
 import { Navigation } from "./Pages/navigation"
-import SearchAndOccupations from './Pages/skillsAndOccupa'
-import SkillsExplorer from './Pages/SearchSkills'
-import OccupationsExplorer from './Pages/SearchOccupations'
+
 
 
 function App() {
 
   return (
 
- 
-<OccupationsExplorer />
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+          <Route path="/login" element={<HandleLogin />} />
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/signup" element={<HandleSignUp />} />
+          <Route path="/About" element={<AboutUs/>}/>
+          <Route path="/Report" element={<ShortReport />} />
+          <Route path="/about" element={<MainLandingPage />} />
+      </Routes>
+      <Footer/> 
+    </BrowserRouter>
+
     
   );
 
@@ -29,15 +38,4 @@ export default App;
 
 
 
-    // <BrowserRouter>
-    //   <Navigation />
-    //   <Routes>
-    //       <Route path="/login" element={<HandleLogin />} />
-    //       <Route path="/" element={<Navigate to="/login" />} />
-    //       <Route path="/signup" element={<HandleSignUp />} />
-    //       <Route path="/About" element={<AboutUs/>}/>
-    //       <Route path="/Report" element={<ShortReport />} />
-    //       <Route path="/about" element={<MainLandingPage />} />
-    //   </Routes>
-    //   <Footer/> 
-    // </BrowserRouter>
+ 
