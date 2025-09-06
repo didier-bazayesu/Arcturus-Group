d2555470998c64fc6f1620cf31428a0c9d340037
dosite-cyiza <dositecyiza@gmail.com>
2025-09-06 18:10:41 +0200

    Responsiveness of connect part

---
+++ src/Component/ConnectPart.jsx
@@ -0,0 +1,18 @@
+export default function ConnectPart (){
+    return (
+        <>
+        <section className="bg-[#032147] text-white p-4 md:p-18 ">
+            <div className="text-center space-y-4 md:space-y-6">
+                <h2 className="text-2xl sm:text-4xl md:text-5xl font-bold ">Let's Connect </h2>
+                <p className="text-sm sm:text-xl">
+                    Whether you're looking to learn more, explore a partnership,
+                     or support our mission to build digital public
+                     infrastructure for youth employment, we’d love to connect.
+                </p>
+            </div>
+
+        </section>
+        
+        </>
+    )
+}
