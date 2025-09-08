588ce20042a159f05c710780d4e7afffa9c9de51
didier-bazayesu <didierbazayesu@gmail.com>
2025-09-08 07:55:25 +0200

    fix: app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import HandleLogin from "./Pages/Login";
 import HandleSignUp from "./Pages/Signup";
-import AboutUs from "./Pages/About";
-// import Footer from "./Pages/Footer"
-import MainLandingPage from "./Pages/MainLandingPage"
-import ShortReport from "./Pages/shortReport"
-import { Navigation } from "./Pages/navigation"
-import SkillsExplorer from './Pages/SearchSkills'
-import  OccupationsExplorer from './Pages/SearchOccupations'
-import SearchAndOccupations from './Pages/skillsAndOccupa'
-import Searchgroup from "./Searchgroup";
-import MainDesign from "./MainDesign";
-
-
 import AboutUs from "./Pages/AboutUs";
-// import Footer from "./Component/Footer";
+import Footer from "./Component/Footer";
 import MainLandingPage from "./Pages/MainLandingPage";
 import ShortReport from "./Pages/shortReport";
 import Navigation from "./Component/navigation";
